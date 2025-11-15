4ef403764d82d8a8ccf98535ea7091b67f02cfeb
Gilbertoknd <gilbertoknd5@gmail.com>
2025-11-15 19:34:22 -0300

    http codes

--- src/server.js
+++ src/server.js
@@ -1,39 +1,42 @@
 import http from 'http'
 
-//Rotas no node.js
 /**
- * CRUD de rotas para o usuário
- * Criar usuários
- * Listar usuários
- * Editar usuários
- * Remover usuários
+ * Trabalhando http status code
+ * Por padrão, as requisições retornam 200: ok.
  * 
- * HTTP
- * * Métodos HTTP: GET / POST / PUT / PATCH / DELETE 
- * * URL
- * 
- * GET para buscar um recurso
- * POST criar recurso
- * PUT editar um recurso
- * PATCH editar uma informação específica de um recurso
- * 
- * Exemplo de diferença entre PUT e PATCH:
- * Ao atualizar por exemplo o perfil inteiro, utilizamos o PUT, podemos mudar várias informações, nome, foto, email.
- * Já o PATCH, por exemplo, se o usuário quer receber uma notificação, ou não, é uma informação específica para ser atualizada.
+ * Códigos de 100 - 199 são respostas informacionais, acabamos não usando
+ * De 200 - 299 são os códigos que indicam sucesso
+ * Entre 300 - 399 são mensagens de redirecionamento
+ * Já 400 - 499 são erros do cliente
+ * E 500 - 509 são erros do servidor, erros inesperados
  */
 
+const users = []
+
 const server = http.createServer((req, res) => {
-  const { method, url } = req
+  const { method, url } = req 
+
+  //Verificando os headers
+  console.log(req.headers)
 
   if (method == 'GET' && url == '/users') {
-    return res.end("Listagem de usuários")
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method == 'POST' && url == '/users') {
-    return res.end("Criação de usuários")
+    users.push({
+      id: 1,
+      name: 'Gilberto',
+      email: 'Gilberto@gmail.com'
+    })
+
+    return res.writeHead(201).end()
   }
 
-  return res.end("Hello World")
+  //Fallback
+  return res.writeHead(404).end("Not Found")
 })
 
 server.listen(3000)
