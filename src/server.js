1520dc75e3ae72668a44eac74a90b365b9dec7ed
Gilbertoknd <gilbertoknd5@gmail.com>
2025-11-26 19:05:46 -0300

    Middleware de json, modularizado

--- src/server.js
+++ src/server.js
@@ -1,36 +1,15 @@
 import http from "http";
-
-/**
- * Trabalhando http status code
- * Por padrão, as requisições retornam 200: ok.
- *
- * Códigos de 100 - 199 são respostas informacionais, acabamos não usando
- * De 200 - 299 são os códigos que indicam sucesso
- * Entre 300 - 399 são mensagens de redirecionamento
- * Já 400 - 499 são erros do cliente
- * E 500 - 509 são erros do servidor, erros inesperados
- */
+import { json } from "./middlewares/json.js";
 
 const users = [];
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
-  const buffers = [];
 
-  for await (const chunk of req) {
-    buffers.push(chunk);
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch {
-    req.body = null;
-  }
+  await json(req, res);
 
   if (method == "GET" && url == "/users") {
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users));
+    return res.end(JSON.stringify(users));
   }
 
   if (method == "POST" && url == "/users") {
